4d6b32dadedf74b50520f9c22a3bc7d89a5b5c10
Promise Fru <promisefru1@gmail.com>
2020-05-09 21:54:23 +0100

    board writer modified ... with overwrite protection added

--- src/main.cpp
+++ src/main.cpp
@@ -27,40 +27,31 @@ int main(){
 		cin >> choice;
 
 		// write board
-		switch (choice){
-				case 1:
-					square[1] = mark;
-					break;
-				case 2:
-					square[2] = mark;
-					break;
-				case 3:
-					square[3] = mark;
-					break;
-				case 4:
-					square[4] = mark;
-					break;
-				case 5:
-					square[5] = mark;
-					break;
-				case 6:
-					square[6] = mark;
-					break;
-				case 7:
-					square[7] = mark;
-					break;
-				case 8:
-					square[8] = mark;
-					break;
-				case 9:
-					square[9] = mark;
-					break;
-				default:
-					{
-						cout << "Invalid entry" << endl;
-						player--;
-					}
-			}
+		if (choice == 1 && square[1] == '1')
+			square[1] = mark;
+		else if (choice == 2 && square[2] == '2')
+			square[2] = mark;
+		else if (choice == 3 && square[3] == '3')
+			square[3] = mark;
+		else if (choice == 4 && square[4] == '4')
+			square[4] = mark;
+		else if (choice == 5 && square[5] == '5')
+			square[5] = mark;
+		else if (choice == 6 && square[6] == '6')
+			square[6] = mark;
+		else if (choice == 7 && square[7] == '7')
+			square[7] = mark;
+		else if (choice == 8 && square[8] == '8')
+			square[8] = mark;
+		else if (choice == 9 && square[9] == '9')
+			square[9] = mark;
+		else
+		{
+			cout<<"Invalid move "<<endl;
+			player--;
+			cin.ignore();
+			cin.get();
+		}
 
 		player++;
 
